f8b3eab6fa9e279b11d0ce375f16cda631debaa6
CLEAR-XI <45460245+CLEAR-XI@users.noreply.github.com>
2019-07-29 20:35:24 +0800

    Add files via upload

---
+++ Static.js
@@ -0,0 +1,13 @@
+setInterval(function(){
+  // document.getElementById("demo").innerHTML=new Date().toLocaleString();
+},1000);
+function send() {
+    var  oes = document.getElementById("get_url").valueOf;
+    Document.getElementById("get_url").setAttribute("value","MEIYOU1")
+}
+var ster = 0;
+function krues() {
+    ster++;
+    alert(ster)
+    document.getElementById("demo").innerHTML = ster;
+}
